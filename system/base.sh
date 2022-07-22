4981691fe0fa79654cc755efe05e9fe70a7809b6
zijun_he <zijun_he@arcplus.com.cn>
2022-07-22 16:44:50 +0800

    feat: centos and docker

---
+++ system/base.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+yum update -y
+
+# ref https://www.cyberithub.com/solved-failed-to-download-metadata-for-repo-appstream/
+cd /etc/yum.repos.d/
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+
+# client operation
+# 
+# connection
+# ssh-copy-id -i id_rsa root@81.68.217.99
+# 
+# ssh config
+# Host 81.68.217.99
+#   HostName 81.68.217.99
+#   IdentityFile ~/.ssh/server_id_rsa
+#   User root
---
+++ system/docker.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+yum remove docker \
+    docker-client \
+    docker-client-latest \
+    docker-common \
+    docker-latest \
+    docker-latest-logrotate \
+    docker-logrotate \
+    docker-engine
+
+yum install -y yum-utils
+
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+# config
+cat >/etc/docker/daemon.json <<EOF
+{
+  "registry-mirrors": [
+    "https://hub-mirror.c.163.com"
+  ]
+}
+EOF
+
+systemctl daemon-reload
+systemctl start docker
+
+# compose
+
+curl -L "https://get.daocloud.io/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
